90ab76ebe8e8b9424d89b8e56af4eab4f35b6a5a
souzajackson <73433159+souzajackson@users.noreply.github.com>
2022-08-02 15:07:19 -0300

    gerador da operação do grupo

--- scripts/game.js
+++ scripts/game.js
@@ -1,57 +1,25 @@
 let toDiscover = 0
 let boardSize = 0
-let answers;
+let generatorRep = new Array(boardSize)
+let nums = new Array(boardSize)
 
 function initGame(size){
     boardSize = size
     toDiscover = size ** 2
-    answers = new Array(boardSize)
-        for(let i = 0; i < boardSize; i++){
-            answers[i] = new Array(boardSize)
-        }
     
     generateOperation()
 }
 
 function generateOperation(){
-    const nums = new Array(boardSize)
     for(let i = 0; i < boardSize; i++){
         nums[i] = i;
     }
-
-    let identidade = Math.floor(Math.random() * nums.length);
     nums.sort(()=> Math.random() - 0.5);
 
-    for(let i = 0; i < boardSize; i++){
-        const inversosCandidatos = new Array(boardSize - i)
-        for(let k = i; k < boardSize; k++){
-            inversosCandidatos[k - i] = k
-        }
-        let inverso = inversosCandidatos[Math.floor(Math.random() * inversosCandidatos.length)]
-        while(inverso == identidade){
-            inverso = inversosCandidatos[Math.floor(Math.random() * inversosCandidatos.length)]
-        }
-
-        for(let j = i; j < boardSize; j++){
-            if(i == identidade){
-                answers[i][j] = j
-                answers[j][i] = j
-            }else if (j == identidade){
-                answers[i][j] = i
-                answers[j][i] = i
-            }else if(j == inverso || i == inverso){
-                answers[i][j] = identidade
-                answers[j][i] = identidade
-            }else{
-                let ans = Math.floor(Math.random() * nums.length);
-                while(ans == identidade){
-                    ans = Math.floor(Math.random() * nums.length);
-                }
-                answers[i][j] = ans
-                answers[j][i] = ans
-            }
-        }
+    for (let i = 0; i < boardSize; i++){
+        generatorRep[nums[i]] = i + 1
     }
+    
 }
 
 function checkMove(x, y){
@@ -63,7 +31,9 @@ function checkMove(x, y){
     if(!selectedImg) return;
     if(square.childNodes.length > 0) return;
 
-    if(answers[x - 1][y - 1] != Number(selectedImg.attributes.value.nodeValue)){
+    let reps = generatorRep[x - 1] + generatorRep[y - 1]
+    if(reps > boardSize) reps -= boardSize
+    if(nums[reps - 1] != Number(selectedImg.attributes.value.nodeValue)){
         playFailSong()
         return;
     }
